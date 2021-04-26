4ef110d81490200c9d2a41634f5f2e30ebe0d3b6
pbullhove <peterbullhove@gmail.com>
2021-04-26 14:21:14 +0200

    developing in process.

--- catkin_ws/src/uav_vision/scripts/complete_mission.py
+++ catkin_ws/src/uav_vision/scripts/complete_mission.py
@@ -14,6 +14,10 @@ current_pose = Twist()
 timer = Timer()
 timeout_timer = Timer()
 current_state = STATE_INIT
+init_complete = False
+landing_complete = False
+phototwirl_complete = False
+photos_taken = 0
 
 
 
@@ -24,18 +28,32 @@ def estimate_callback(data):
     global current_pose
     current_pose = data
 
+def landing_complete_callback(data):
+    global landing_complete
+    landing_complete = True
+
+def init_complete_callback(data):
+    global init_complete
+    init_complete = True
 
 
 def main():
     rospy.init_node('complete_mission',anonymous=True)
 
 
+    #pub_init_drone = rospy.Publisher('/')
+    pub_desired_pose = rospy.Publisher("/set_point", Twist, queue_size=1)
+    pub_start_takeoff = rospy.Publisher("/ardrone/takeoff", Empty, queue_size=10)
+    pub_start_automated_landing = rospy.Publisher('/initiate_automated_landing', Empty, queue_size=1)
+    pub_save_front_camera_photo = rospy.Publisher('/take_still_photo_front',Empty, queue_size=1)
+
+    rospy.Subscriber('/estimate/dead_reckoning', Twist, estimate_callback)
+    rospy.Subscriber('/ardrone/land', Empty, landing_complete_callback)
+
+
+
     while not rospy.is_shutdown():
-        if not timer.is_timeout():
-            pass
-        else:
-            print('timer timeout')
-            break
+        pass
 
 
 
