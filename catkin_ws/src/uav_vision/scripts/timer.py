8e86fdd0880f7d3f219780df44b0f089b4d90cb1
pbullhove <peterbullhove@gmail.com>
2021-04-26 13:11:35 +0200

    created timer class

---
+++ catkin_ws/src/uav_vision/scripts/timer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+
+# timer.py
+import datetime as dt
+
+class TimerError(Exception):
+    """A custom exception used to report errors in use of Timer class"""
+
+class Timer:
+    def __init__(self):
+        self._start_time = None
+        self._duration = None
+
+    def start(self, duration=None):
+        """Start a new timer"""
+        self._duration = duration
+        if self._start_time is not None:
+            raise TimerError("Timer is running. Use .stop() to stop it")
+
+        self._start_time = dt.datetime.now()
+
+    def stop(self):
+        """Stop the timer, and report the elapsed time"""
+        if self._start_time is None:
+            raise TimerError("Timer is not running. Use .start() to start it")
+
+        elapsed_time = (dt.datetime.now() - self._start_time).total_seconds()
+        self._start_time = None
+        return elapsed_time
+
+
+    def is_timeout(self):
+        """Checks whether the timer has been running longer than the allotted duration. """
+        if self._duration is not None:
+            return (dt.datetime.now() - self._start_time).total_seconds() > self._duration
+        else:
+            return False
