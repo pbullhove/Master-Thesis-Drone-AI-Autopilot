d2c6c32925156d10d64ce268f3b0f3e99fa63e56
pbullhove <peterbullhove@gmail.com>
2021-04-27 16:00:29 +0200

    now inits when connected to relevant nodes. FSM complete

--- catkin_ws/src/uav_vision/scripts/complete_mission.py
+++ catkin_ws/src/uav_vision/scripts/complete_mission.py
@@ -22,12 +22,12 @@ desired_pose = Twist()
 desired_pose.linear.z = 3
 current_pose = Twist()
 timer = Timer()
-timeout_timer = Timer()
+error_timer = Timer()
 state = STATE_INIT
-init_complete = False
 landing_complete = False
 phototwirl_complete = False
 photos_taken = 0
+received_estimate = False
 
 
 
@@ -36,9 +36,9 @@ photos_taken = 0
 #############
 def estimate_callback(data):
     global current_pose
-    global init_complete
+    global received_estimate
     #print('received estimate')
-    init_complete = True
+    received_estimate = True
     current_pose = data
 
 def landing_complete_callback(data):
@@ -67,10 +67,11 @@ def close_enough(pose_a, pose_b):
     return ang < 5 and euc < 0.1
 
 
+
 def main():
     rospy.init_node('complete_mission',anonymous=True)
 
-    global init_complete
+    global received_estimate
     global landing_complete
     global desired_pose
     global phototwirl_complete
@@ -81,17 +82,29 @@ def main():
     pub_start_takeoff = rospy.Publisher("/ardrone/takeoff", Empty, queue_size=10)
     pub_start_automated_landing = rospy.Publisher('/initiate_automated_landing', Empty, queue_size=1)
     pub_save_front_camera_photo = rospy.Publisher('/take_still_photo_front',Empty, queue_size=1)
-
+    control_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    pid_off = rospy.Publisher('/pid_on_off', Bool, queue_size=1)
     rospy.Subscriber('/estimate/dead_reckoning', Twist, estimate_callback)
     rospy.Subscriber('/ardrone/land', Empty, landing_complete_callback)
 
+    def init_complete():
+        a = pub_desired_pose.get_num_connections() > 0
+        b = pub_start_takeoff.get_num_connections() > 0
+        c = pub_start_automated_landing.get_num_connections() > 0
+        d = pub_save_front_camera_photo.get_num_connections() > 0
+        e = control_pub.get_num_connections() > 0
+        return all([a, b, c, d, e, received_estimate])
 
     timer.start(1)
     print('State is:', state)
     while not rospy.is_shutdown():
+        if state not in [STATE_ERROR, STATE_IDLE, STATE_INIT] and error_timer.is_timeout():
+            state = STATE_ERROR
+
         if state == STATE_INIT:
-            if init_complete and timer.is_timeout():
-                timer.stop()
+            if init_complete() and timer.is_timeout():
+                error_timer.start(100000)
+                error_timer.reset()
                 state = STATE_TAKEOFF
                 print('switching to: ', state)
                 pub_start_takeoff.publish(empty)
@@ -99,12 +112,14 @@ def main():
 
         if state == STATE_TAKEOFF:
             if close_enough(current_pose, desired_pose):
+                error_timer.reset()
                 print('ye close enough')
                 state = STATE_HOVER
                 print('switching to: ', state)
 
         if state == STATE_HOVER:
             if close_enough(current_pose, desired_pose):
+                error_timer.reset()
                 try:
                     timer.start(5)
                 except TimerError as t:
@@ -120,21 +135,33 @@ def main():
 
         if state == STATE_PHOTOTWIRL:
             if close_enough(current_pose, desired_pose):
-                pub_save_front_camera_photo.publish(empty)
-                photos_taken += 1
-                #desired_pose.angular.z = [135, 45, -45, -135][photos_taken]
-                desired_pose.angular.z = [0, 90, 179, -90, 0][photos_taken]
-                pub_desired_pose.publish(desired_pose)
-                if photos_taken == 4:
+                if photos_taken < 4:
+                    error_timer.reset()
+                    pub_save_front_camera_photo.publish(empty)
+                    photos_taken += 1
+                    #desired_pose.angular.z = [135, 45, -45, -135][photos_taken]
+                    desired_pose.angular.z = [0, 90, 179, -90, 0][photos_taken]
+                    pub_desired_pose.publish(desired_pose)
+                else:
                     state = STATE_HOVER
                     print('switching to: ', state)
                     phototwirl_complete = True
 
         if state == STATE_LANDING:
             if landing_complete:
+                error_timer.stop()
                 state = STATE_IDLE
                 print('switching to: ', state)
 
+        if state == STATE_ERROR:
+            print('switching to: ', state)
+            msg = Twist()
+            off = Bool()
+            off.data = False
+            msg.linear.z = -0.3
+            while True:
+                pid_off.publish(off)
+                control_pub.publish(msg)
 
 if __name__ == "__main__":
     main()
