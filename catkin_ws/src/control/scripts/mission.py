1318d8c86c29bd5c6e8fc381acbe502f4b72e3bf
pbullhove <peterbullhove@gmail.com>
2021-05-06 14:55:58 +0200

    added mission moveabit

--- catkin_ws/src/control/scripts/mission.py
+++ catkin_ws/src/control/scripts/mission.py
@@ -46,9 +46,11 @@ else:
     elif args[-1].lower() == "tmovefarl":
         mission_plan = ["INIT", "TAKEOFF", "MOVE TO [10,10,10,0,0,90]", "HOVER", "MOVE TO [0,0,3,0,0,0]", "HOVER", "LANDING"]
     elif args[-1].lower() == "photomission_here":
-        mission_plan = ["INIT", "TAKEOFF", "HOVER", "HOVER", "PHOTOTWIRL", "PHOTOTWIRL", "MOVE TO [0,0,3,0,0,0]", "HOVER", "LANDING", "IDLE"]
+        mission_plan = ["INIT", "TAKEOFF", "HOVER", "HOVER", "PHOTOTWIRL", "MOVE TO [0,0,3,0,0,0]", "HOVER", "LANDING", "IDLE"]
     elif args[-1].lower() == "photomission_there":
         mission_plan = ["INIT", "TAKEOFF", "HOVER", "MOVE TO [2,0,3,0,0,0]", "HOVER", "PHOTOTWIRL", "HOVER", "MOVE TO [0,0,3,0,0,0]", "HOVER", "LANDING", "IDLE"]
+    elif args[-1].lower() == "moveabit":
+        mission_plan = ["INIT", "TAKEOFF", "HOVER", "MOVE TO [1,1,2,0,0,0]", "HOVER", "MOVE TO [0,0,2,0,0,0]", "HOVER", "LANDING", "IDLE"]
     else:
         raise("unknown mission")
 
