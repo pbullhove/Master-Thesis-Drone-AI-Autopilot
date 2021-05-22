4646a7657a9ff275c1b2529bec70796ac66a7be3
pbullhove <peterbullhove@gmail.com>
2021-05-22 11:56:12 +0200

    data collection updated to master_thesis

--- catkin_ws/src/utilities/scripts/data_collection.py
+++ catkin_ws/src/utilities/scripts/data_collection.py
@@ -23,99 +23,15 @@ global_collect_data = False
 received_signal = False
 start_time = None
 
+
+
+def to_array(twist):
+    return np.array([twist.linear.x, twist.linear.y, twist.linear.z, twist.angular.x, twist.angular.y, twist.angular.z])
 #############
 # Callbacks #
 #############
-global_est_ground_truth = np.zeros(6)
-def ground_truth_callback(data):
-    global global_est_ground_truth
-    global_est_ground_truth = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
 
-# Estimate callbacks
-global_est_ellipse = np.zeros(6)
-def estimate_ellipse_callback(data):
-    global global_est_ellipse
-    global_est_ellipse = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-#
-global_est_arrow = np.zeros(6)
-def estimate_arrow_callback(data):
-    global global_est_arrow
-    global_est_arrow = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-#
-global_est_corners = np.zeros(6)
-def estimate_corners_callback(data):
-    global global_est_corners
-    global_est_corners = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-#
-global_est_dead_reckoning = np.zeros(6)
-def estimate_dead_reckoning_callback(data):
-    global global_est_dead_reckoning
-    global_est_dead_reckoning = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-######################
-
-# Estimate error callbacks
-global_est_error_ellipse = np.zeros(6)
-def estimate_error_ellipse_callback(data):
-    global global_est_error_ellipse
-    global_est_error_ellipse = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-#
-global_est_error_arrow = np.zeros(6)
-def estimate_error_arrow_callback(data):
-    global global_est_error_arrow
-    global_est_error_arrow = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-#
-global_est_error_corners = np.zeros(6)
-def estimate_error_corners_callback(data):
-    global global_est_error_corners
-    global_est_error_corners = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-#
-global_est_error_dead_reckoning = np.zeros(6)
-def estimate_error_dead_reckoning_callback(data):
-    global global_est_error_dead_reckoning
-    global_est_error_dead_reckoning = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-
-#
-global_est_tcv = np.zeros(6)
-def est_tcv_callback(data):
-    global global_est_tcv
-    global_est_tcv = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-
-
-global_filtered_estimate = np.zeros(6)
-def filtered_estimate_callback(data):
-    global global_filtered_estimate
-    global_filtered_estimate = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-
-
-global_est_yolo = np.zeros(6)
-def estimate_yolo_callback(data):
-    global global_est_yolo
-    global_est_yolo = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-
-global_est_error_yolo = np.zeros(6)
-def estimate_error_yolo_callback(data):
-    global global_est_error_yolo
-    global_est_error_yolo = np.array([data.linear.x, data.linear.y, data.linear.z, 0, 0, data.angular.z])
-######################
-
-
-# def start_data_collection_callback(data):
-#     global global_collect_data
-#     global received_signal
-#     global start_time
-#     global_collect_data = True
-#     received_signal = True
-#     start_time = rospy.get_time()
-#     rospy.loginfo("Start data collection")
-#
-#
-# def stop_data_collection_callback(data):
-#     global global_collect_data
-#     global_collect_data = False
-#     stop_time = rospy.get_time()
-#     rospy.loginfo("Stop data collection")
-
-def data_collection_callback(data):
+def toggle_data_collection(data):
     global global_collect_data
     global received_signal
     global start_time
@@ -124,40 +40,78 @@ def data_collection_callback(data):
     start_time = rospy.get_time() if start_time == None else start_time
 
 
-def main(test_number):
+ground_truth = np.zeros(6)
+def ground_truth_callback(data):
+    global global_est_ground_truth
+    ground_truth = to_array(data)
+
+filtered_estimate = np.zeros(6)
+filtered_error = np.zeros(6)
+def filtered_estimate_callback(data):
+    global filtered_estimate
+    filtered_estimate = to_array(data)
+
+tcv = np.zeros(6)
+tcv_error = np.zeros(6)
+def est_tcv_callback(data):
+    global tcv
+    tcv = to_array(data)
+
+dnnCV = np.zeros(6)
+dnnCV_error = np.zeros(6)
+def estimate_dnncv_callback(data):
+    global dnnCV
+    dnnCV = to_array(data)
+
+barometer = np.zeros(6)
+barometer_error = np.zeros(6)
+def estimate_barometer_callback(data):
+    global barometer
+    barometer = np.array(data)
+
+gps = np.zeros(6)
+gps_error = np.zeros(6)
+def estimate_gps_callback(data):
+    global gps
+    gps = to_array(data)
+
+imu = np.zeros(6)
+imu_error = np.zeros(6)
+def estimate_imu_callback(data):
+    global imu
+    imu = to_array(data)
+
+set_point = np.zeros(6)
+set_point_error = np.zeros(6)
+def set_point_callback(data):
+    global set_point
+    set_point = to_array(data)
+
+
+
+
+def main():
     global global_state
     global start_time
     rospy.init_node('planner', anonymous=True)
 
+    rospy.Subscriber('/start_data_collection', Empty, toggle_data_collection)
+    rospy.Subscriber('/set_point', Twist, set_point_callback)
     rospy.Subscriber('/drone_ground_truth', Twist, ground_truth_callback)
-
-    rospy.Subscriber('/estimate/ellipse', Twist, estimate_ellipse_callback)
-    rospy.Subscriber('/estimate/arrow', Twist, estimate_arrow_callback)
-    rospy.Subscriber('/estimate/corners', Twist, estimate_corners_callback)
-    rospy.Subscriber('/estimate/dead_reckoning', Twist, estimate_dead_reckoning_callback)
-    rospy.Subscriber('/estimate/tcv_estimate', Twist, est_tcv_callback)
-
-
-    rospy.Subscriber('/estimate_error/ellipse', Twist, estimate_error_ellipse_callback)
-    rospy.Subscriber('/estimate_error/arrow', Twist, estimate_error_arrow_callback)
-    rospy.Subscriber('/estimate_error/corners', Twist, estimate_error_corners_callback)
-    rospy.Subscriber('/estimate_error/dead_reckoning', Twist, estimate_error_dead_reckoning_callback)
-
     rospy.Subscriber('/filtered_estimate', Twist, filtered_estimate_callback)
 
-    # rospy.Subscriber('/initiate_mission', Empty, start_data_collection_callback)
-    # rospy.Subscriber('/take_still_photo', Empty, stop_data_collection_callback)
-    rospy.Subscriber('/start_data_collection', Empty, data_collection_callback)
-
-    rospy.Subscriber('/estimate/yolo_estimate', Twist, estimate_yolo_callback)
-    rospy.Subscriber('/estimate_error/yolo_error', Twist, estimate_error_yolo_callback)
+    rospy.Subscriber('/estimate/tcv_estimate', Twist, est_tcv_callback)
+    rospy.Subscriber('/estimate/dnnCV', Twist, estimate_dnncv_callback)
+    rospy.Subscriber('/estimate/barometer', Twist, estimate_barometer_callback)
+    rospy.Subscriber('/estimate/gps', Twist, estimate_gps_callback)
+    rospy.Subscriber('/estimate/imu', Twist, estimate_imu_callback)
 
     pub_heartbeat = rospy.Publisher("/heartbeat_data_collection", Empty, queue_size=10)
     heartbeat_msg = Empty()
 
     rospy.loginfo("Starting data collection.. Make sure this terminal window is in /master_thesis/.")
     time.sleep(1)
-    rospy.loginfo("... Ready! Press d in key teleop to start.")
+    rospy.loginfo("... Ready! Press d in manual control to start.")
 
     data_array = []
 
@@ -170,27 +124,32 @@ def main(test_number):
         if global_collect_data:
             curr_time = rospy.get_time() - start_time
 
-            data_point = np.concatenate((
-                # Time
-                np.array([curr_time]),
-                # Ground truth
-                global_est_ground_truth,
-                # Estimate
-                # global_est_ellipse,
-                # global_est_arrow,
-                # global_est_corners,
-                # global_est_dead_reckoning,
-                global_est_yolo,
-                global_est_tcv,
+            set_point_error = set_point - ground_truth
+            filtered_error = ground_truth - filtered_estimate
+            tcv_error = ground_truth - tcv
+            dnnCV_error = ground_truth - dnnCV
+            gps_error = ground_truth - gps
+            barometer_error = ground_truth - barometer
+            imu_error = ground_truth - imu
 
-                # Estimate errors
-                # global_est_error_ellipse,
-                # global_est_error_arrow,
-                # global_est_error_corners,
-                # global_est_error_dead_reckoning,
-                global_est_error_yolo,
-                # Filtered estimate
-                global_filtered_estimate
+
+            data_point = np.concatenate((
+                np.array([curr_time]),
+                ground_truth,
+                set_point,
+                filtered_estimate,
+                tcv,
+                dnnCV,
+                gps,
+                barometer,
+                imu,
+                set_point_error,
+                filtered_error,
+                tcv_error,
+                dnnCV_error,
+                gps_error,
+                barometer_error,
+                imu_error
                 )
             )
             # print len(data_array)
@@ -207,7 +166,7 @@ def main(test_number):
         pub_heartbeat.publish(heartbeat_msg)
 
         rate.sleep()
-    folder = './catkin_ws/src/uav_vision/data_storage/'
+    folder = './catkin_ws/src/utilities/data_storage/'
 
 
     filename = 'unnamed.npy'
@@ -216,7 +175,4 @@ def main(test_number):
 
 
 if __name__ == '__main__':
-    # 1 is hovering with Thomas' algorithm
-    test_number = 1
-
-    main(test_number)
+    main()
